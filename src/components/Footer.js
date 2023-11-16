11488c197d56b0db245f49ed4eed3a251553bc2d
Leul Tewolde <leulwoldegabriel@gmail.com>
2023-11-15 22:28:50 -0600

    Added semantic HTML and og metadata

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; Little Lemon . Coursera . Leul Tewolde . 2023</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import logo from '../logo.jpg';
+
+function Header() {
+    return <header>
+        <img src={logo} alt='App Header Logo' width='202'/>
+    </header>;
+}
+
+export default Header;
